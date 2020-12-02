e4eddf7f98da18f037af45df3ac80f6a7b9853e8
sergeyleontiev <serejatrapecija@gmail.com>
2020-12-02 13:39:02 +0300

    5 итерация

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const input = document.querySelectorAll('.container__form-input[id=fname]');
+const errorTxt = document.querySelector('.errorText');
+const userName = document.querySelector('.name');
+for(let item of [...input]) {
+    item.onblur = function() {
+        errorTxt.style.display = 'block';
+    };
+    item.onfocus = function() {
+        errorTxt.style.display = 'none';
+    }
+    item.onchange = function() {
+        userName.textContent = item.value;
+        item.value = '';
+    };
+}
+
 const title = document.querySelector('.wrapper-title');
 title.addEventListener('click', (e) => {
     if(e.target.textContent === 'Personal'){
@@ -11,7 +27,7 @@ const btnForm = document.querySelector('.container__form-button');
 const clsPopup = document.querySelector('.clsPopup');
 const popup = document.querySelector('.popup');
 btnForm.addEventListener('click', (e) => {
-    e.preventDefault();
+    console.log(e);
     popup.style.display = 'block';
 });
 clsPopup.addEventListener('click', (e) => {
@@ -44,6 +60,101 @@ list.addEventListener('mousemove', () => {
         errorText.textContent = 'Либо массив пуст, либо отсутствует';
     }
 });
+
+// 5 итерация
+
+class User {
+    constructor(name, age, isFrontendDeveloper, skills, additionalSkills) {
+        this.name = name;
+        this.age = age;
+        this.isFrontendDeveloper = isFrontendDeveloper;
+        this.skills = skills;
+        this.additionalSkills = additionalSkills;
+    }
+
+    setAdditionalSkills(...params) {
+        for(let item of params) {
+            this.additionalSkills.push(item);
+        }
+    }
+
+    setName () {
+        let name = prompt('Введите Ваше имя', 'default');
+        this.name = name;
+    }
+
+    getName () {
+        console.log(`Здравствуйте, ${this.name}`);
+    }
+
+}
+
+class Subuser extends User{
+    constructor(name, age, isFrontendDeveloper, skills, additionalSkills, workPlace) {
+        super(name, age, isFrontendDeveloper, skills, additionalSkills);
+        this.workPlace = workPlace;
+    }
+}
+const subuser = new Subuser(
+    'Ivan',
+    26,
+    true,
+    {
+        HTML: true,
+        CSS: true,
+        PHP: true,
+        JavaScript: true,
+        Vue: true,
+        Angular: false,
+        React: false,
+    },
+    [
+        'GIT',
+        'Jira',
+    ],
+    'Apple',
+    );
+console.log(subuser);
+
+const user = new User(
+    'Sergey',
+    23,
+    true,
+    {
+        HTML: true,
+        CSS: true,
+        PHP: true,
+        JavaScript: true,
+        Vue: true,
+        Angular: false,
+        React: false,
+    },
+
+    [
+        'GIT',
+        'Jira',
+    ]
+);
+console.log(User.prototype.__proto__);
+
+const {name, age, skills} = user;
+console.log(name, age, skills);
+
+function* skillsDev() {
+    for(let item of Object.keys(skills)) {
+        yield item;
+    }
+}
+const skill = skillsDev();
+const skillArr = [];
+for(let item of skill) {
+    skillArr.push(item);
+}
+console.log(skillArr);
+
+
+
+
 
 
 
